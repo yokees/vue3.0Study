661eb96b8c54a49487bfaa236db5bfba8281bfb3
admin <yokees@vip.qq.com>
2018-09-08 21:25:29 +0800

    helloworld组件v-for的使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,27 +6,21 @@
     </p>
     <h3>安装 CLI 插件</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-nightwatch" target="_blank" rel="noopener">e2e-nightwatch</a></li>
+      <li v-for="(cli,index) in clis" :key="index">
+        <a :href=linkPrefix+cli.link target="_blank" rel="noopener">{{cli.text}}</a>
+      </li>
     </ul>
     <h3>vue 相关生态</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">核心文档</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">官方论坛</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">社交聊天</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">资讯</a></li>
+      <li v-for="(vue,index) in vues1" :key="index">
+        <a :href=vue.link target="_blank" rel="noopener">{{vue.text}}</a>
+      </li>
     </ul>
     <h3>vue 周边组件</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(vue,index) in vues2" :key="index">
+        <a :href=vue.link target="_blank" rel="noopener">{{vue.text}}</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -36,6 +30,32 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function () {
+    return {
+      linkPrefix: 'https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-',
+      clis: [
+        { link: 'babel', text: 'babel' },
+        { link: 'pwa', text: 'pwa' },
+        { link: 'eslint', text: 'eslint' },
+        { link: 'unit-mocha', text: 'unit-mocha' },
+        { link: 'e2e-nightwatch', text: 'e2e-nightwatch' }
+      ],
+      vues1: [
+        { link: 'https://vuejs.org', text: '核心文档' },
+        { link: 'https://forum.vuejs.org', text: '官方论坛' },
+        { link: 'https://chat.vuejs.org', text: '社交聊天' },
+        { link: 'https://twitter.com/vuejs', text: 'Twitter' },
+        { link: 'https://news.vuejs.org', text: '资讯' }
+      ],
+      vues2: [
+        { link: 'https://router.vuejs.org', text: 'vue-router' },
+        { link: 'https://vuex.vuejs.org', text: 'vuex' },
+        { link: 'https://github.com/vuejs/vue-devtools#vue-devtools', text: 'vue-devtools' },
+        { link: 'https://vue-loader.vuejs.org', text: 'vue-loader' },
+        { link: 'https://github.com/vuejs/awesome-vue', text: 'awesome-vue' }
+      ]
+    }
   }
 }
 </script>
