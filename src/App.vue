3ee91537048dbf307b6c9578f2ac336d7569e1b6
admin <yokees@vip.qq.com>
2018-09-09 21:01:13 +0800

    使用网络API数据简单重写home组件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">主页</router-link> |
-      <router-link to="/about">关于</router-link>
+      <router-link to="/">主页</router-link>
+      <router-link to="/about">VUE</router-link>
+      <span>
+        <a href="https://www.cnblogs.com/ndos/" target="_blank">cnblogs</a>
+        <a href="https://github.com/yokees/vue3.0Study" target="_blank">Github</a>
+      </span>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+}
+html, body, #app {
+  width: 100%;
+  height:100%;
+}
+a {
+  text-decoration-line: none;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 0 20px;
+  font-size: 20px;
+  height:60px;
+  line-height: 60px;
+  background: rgba(125,125,0,0.3);
   a {
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
     }
+    margin: 0 5px;
+  }
+  span {
+    float: right;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="欢迎来到 nDos 博客，下面是 vue 官方相关资料的链接"/>
+    <div class="nav">
+      <div v-for="(value, key, index) in navs" :key="index">
+        {{key}}
+      </div>
+    </div>
+    <ul>
+      <li v-for="(tt,index) in tts" :key="index">
+        {{tt.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      navs: {},
+      tts: []
+    }
+  },
+  created: function () {
+    fetch('https://www.apiopen.top/journalismApi')
+      .then(v => v.json())
+      .then(v => {
+        console.log(v.data)
+        this.tts = v.data.toutiao
+        this.navs = v.data
+      })
   }
 }
 </script>
+
+<style lang="less" scoped>
+.home {
+  height: calc( 100% - 60px );
+  .nav {
+    width: 80px;
+    height: calc( 100% - 30px );
+    padding: 15px 10px;
+    float: left;
+    box-shadow: inset 0 10px 10px -8px #333;
+  }
+  ul {
+    width: calc( 100% - 100px );
+    height: calc( 100% - 30px );
+    overflow: hidden;
+    padding: 15px 0;
+    float: right;
+    box-shadow: inset 0 10px 10px -8px #333;
+    li {
+      height: 30px;
+      line-height: 30px;
+    }
+  }
+}
+</style>
