d8c2b4251c93af1f9ae27c6cfd9f5c71df973d4d
admin <yokees@vip.qq.com>
2018-09-13 21:39:55 +0800

    局部注册组件

---
+++ src/components/Base/TopNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="topnav">
+      {{title}}
+  </span>
+</template>
+
+<script>
+
+export default {
+  name: 'topnav',
+  props: ['title'],
+  data: function () {
+    return {
+      text: '导航条'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+span { margin: 0 5px; }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,20 @@
-<template>
-  <div class="about">
-    <HelloWorld msg="vue 官方相关资料的链接"/>
-  </div>
-</template>
+<template><div class="about">
+  <top-nav title="推荐"/>
+  <top-nav title="军事"/>
+  <top-nav title="社会"/>
+  <top-nav title="科技"/>
+  <HelloWorld msg="vue 官方相关资料的链接"/>
+</div></template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TopNav from '@/components/Base/TopNav.vue'
 
 export default {
-  name: 'about',
+  name: 'home',
   components: {
-    HelloWorld
+    HelloWorld, TopNav
   }
 }
 </script>
